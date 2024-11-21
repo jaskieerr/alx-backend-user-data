9e72da335d08553560b246a819de1000cb7cf3d4
jaskieerr <nizar.zairat@gmail.com>
2024-11-21 03:23:50 +0000

    pycode

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -15,7 +15,7 @@ class DB:
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
-        
+
     @property
     def _session(self) -> Session:
         """Memoized session object"""
@@ -23,7 +23,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         '''adduser'''
         user = User(email=email, hashed_password=hashed_password)
