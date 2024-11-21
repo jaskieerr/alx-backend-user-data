bbe1f86c3d58de70a0b8c9e768a6d289571a9a21
jaskieerr <nizar.zairat@gmail.com>
2024-11-21 03:28:38 +0000

    updateuser

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,3 +42,13 @@ class DB:
         except InvalidRequestError:
             raise
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        '''update user'''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
